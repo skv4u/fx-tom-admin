a6ea894f06786bad87115ecd8216820595fede31
Alam Bhanu <alam.bhanu@idsnext.space>
2022-03-26 19:15:13 +0530

    changes added

--- src/app/private/dashboard/comments/comments.component.ts
+++ src/app/private/dashboard/comments/comments.component.ts
@@ -9,90 +9,138 @@ import { WebService } from 'src/app/shared/services/web.service';
   styleUrls: ['./comments.component.scss']
 })
 export class CommentsComponent implements OnInit {
-  CommentsList:any=[];
-  commentText:any ;
-  replycomment:string=""
-  constructor(public prodcastService:ProdcastService,public webService:WebService,public LocalStorage:LocalstorageService) { }
+  CommentsList: any = [];
+  commentText: any;
+  replycomment: string = ""
+  constructor(public prodcastService: ProdcastService, public webService: WebService, public LocalStorage: LocalstorageService) { }
 
   ngOnInit() {
     this.getCommentList();
   }
-  getCommentList(){
+  getCommentList() {
     this.prodcastService.loader = true;
-    let req={
+    let req = {
       "podcast_id": this.prodcastService.selectedData.id,
       "user_id": this.LocalStorage.getUserData().id
-      };
-      this.webService.commonMethod('mobuser/podcast/commentreplylist',req,'POST').subscribe(
-        (data)=>{
-          console.log(data);
-          this.prodcastService.loader = false;
-          this.CommentsList=data.Response;
-          for(let a of this.CommentsList){
-            a.replycomment = "";
-          }
+    };
+    this.webService.commonMethod('mobuser/podcast/commentreplylist', req, 'POST').subscribe(
+      (data) => {
+        console.log(data);
+        this.prodcastService.loader = false;
+        this.CommentsList = data.Response;
+        for (let a of this.CommentsList) {
+          a.replycomment = "";
         }
-      )
-  }
-
-  LikeandHeart(id,type){
-    this.prodcastService.loader=true;
-    let req={
-      "comment_id":id,
-      "user_id":this.LocalStorage.getUserData().id,
-      "type":type,
-      "created_by":this.LocalStorage.getUserData().username
+      },
+      err => {
+        this.prodcastService.loader = false;
       }
-    
-      this.webService.commonMethod('podcast/commentldh',req,'POST').subscribe(
-        (data)=>{
-          console.log(data);
-          this.prodcastService.loader=false;
-          if(data.Status == "Success" && data.Response)
-          this.getCommentList();
-
-        }
-      )
+    )
   }
 
-  replyComment(i,id,comment){
-    this.prodcastService.loader=true;
-    let req={
-      "comment_id":id,
-      "user_id":this.LocalStorage.getUserData().id,
-      "description":comment
-      };
-      this.webService.commonMethod('podcast/comment/reply',req,'POST').subscribe(
-        (data)=>{
-          console.log(data);
-          this.prodcastService.loader=false;
-          if(data.Status == 'Success' && data.Response != ''){
-            this.CommentsList[i].replycomment = "";
-          this.CommentsList=data.Response;
-          this.getCommentList();
-          }
-        }
-      )
-  }
-
-  createCommets(){
+  LikeandHeart(id, type) {
     this.prodcastService.loader = true;
-    console.log("commentText",this.commentText);
-    let req={
-      "podcast_id":this.prodcastService.selectedData.id,
-      "user_id":this.LocalStorage.getUserData().id,
-      "description":this.commentText,
-      "filepath":"",
-      "created_by":this.LocalStorage.getUserData().username
-      };
-      this.webService.commonMethod('podcast/comment',req,'POST').subscribe(
-        (data)=>{
-          this.prodcastService.loader = false;
-          this.commentText = "";
-          document.getElementById('commenttext').innerHTML="";
+    let req = {
+      "comment_id": id,
+      "user_id": this.LocalStorage.getUserData().id,
+      "type": type,
+      "created_by": this.LocalStorage.getUserData().username
+    }
+
+    this.webService.commonMethod('podcast/commentldh', req, 'POST').subscribe(
+      (data) => {
+        console.log(data);
+        this.prodcastService.loader = false;
+        if (data.Status == "Success" && data.Response)
           this.getCommentList();
-          // this.CommentsList=data.Response;
+
+      },
+      err => {
+        this.prodcastService.loader = false;
+      }
+    )
+  }
+
+  replyComment(i, id, comment) {
+    this.prodcastService.loader = true;
+    let req = {
+      "comment_id": id,
+      "user_id": this.LocalStorage.getUserData().id,
+      "description": comment
+    };
+    this.webService.commonMethod('podcast/comment/reply', req, 'POST').subscribe(
+      (data) => {
+        console.log(data);
+        this.prodcastService.loader = false;
+        if (data.Status == 'Success' && data.Response != '') {
+          this.CommentsList[i].replycomment = "";
+          this.CommentsList = data.Response;
+          this.getCommentList();
         }
-      )
+      },
+      err => {
+        this.prodcastService.loader = false;
+      }
+    )
+  }
+
+  createCommets() {
+    this.prodcastService.loader = true;
+    console.log("commentText", this.commentText);
+    let req = {
+      "podcast_id": this.prodcastService.selectedData.id,
+      "user_id": this.LocalStorage.getUserData().id,
+      "description": this.commentText,
+      "filepath": "",
+      "created_by": this.LocalStorage.getUserData().username
+    };
+    this.webService.commonMethod('podcast/comment', req, 'POST').subscribe(
+      (data) => {
+        this.prodcastService.loader = false;
+        this.commentText = "";
+        document.getElementById('commenttext').innerHTML = "";
+        this.getCommentList();
+        // this.CommentsList=data.Response;
+      },
+      err => {
+        this.prodcastService.loader = false;
+      }
+    )
+  }
+
+
+  deleteCommets(id) {
+    this.prodcastService.loader = true;
+    let req = {
+      "comment_id": id,
+      "user_id": this.LocalStorage.getUserData().id
+    }
+    this.webService.commonMethod('mobuser/comment', req, 'DELETE').subscribe(
+      (data) => {
+        this.prodcastService.loader = false;
+        if (data.Status == 'Success' && data.Response)
+          this.getCommentList();
+      },
+      err => {
+        this.prodcastService.loader = false;
+      }
+    )
+  }
+  deleteReply(id) {
+    this.prodcastService.loader = true;
+    let req = {
+      "reply_id": id,
+      "user_id": this.LocalStorage.getUserData().id
+    }
+    this.webService.commonMethod('mobuser/comment/reply', req, 'DELETE').subscribe(
+      (data) => {
+        this.prodcastService.loader = false;
+        if (data.Status == 'Success' && data.Response)
+          this.getCommentList();
+      },
+      err => {
+        this.prodcastService.loader = false;
+      }
+    )
   }
 }
