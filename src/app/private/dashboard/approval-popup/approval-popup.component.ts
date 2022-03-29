bd96682e26cb01003498b876bca57aceef68e263
Santosh <santosh.v@idsnext.com>
2022-03-29 23:42:43 +0530

    Hour and min

--- src/app/private/dashboard/approval-popup/approval-popup.component.ts
+++ src/app/private/dashboard/approval-popup/approval-popup.component.ts
@@ -12,24 +12,24 @@ export class ApprovalPopupComponent implements OnInit {
   notes: string = "";
   broadCastDate: string = "";
   broadCastTime: string = "";
-  hh:string = "00";
-  mm:string = "00";
+  hh: string = "00";
+  mm: string = "00";
   constructor(public prodcastService: ProdcastService, public webService: WebService, public toast: ToastService) { }
 
   ngOnInit() {
-    if(this.prodcastService.showPopUp.broadcast){
-      try{
-        let part=this.prodcastService.selectedData.broadcast_date_actual.split(' ');
-        this.broadCastDate=part[0];
-        this.broadCastTime=part[1];
+    if (this.prodcastService.showPopUp.broadcast) {
+      try {
+        let part = this.prodcastService.selectedData.broadcast_date_actual.split(' ');
+        this.broadCastDate = part[0];
+        this.broadCastTime = part[1];
         let timepart = this.broadCastTime.split(':')
-        this.hh=timepart[0];
-        this.mm=timepart[1];
+        this.hh = timepart[0];
+        this.mm = timepart[1];
       }
-      catch(er){
+      catch (er) {
         console.log("date issue")
       }
-     
+
     }
   }
   approveProdCast(status) {
@@ -47,7 +47,7 @@ export class ApprovalPopupComponent implements OnInit {
       "status": status,
       "created_by": this.prodcastService.loginUserName,
       "audio_path": this.prodcastService.selectedData.audiopath,
-      "podcast_name":this.prodcastService.selectedData.name
+      "podcast_name": this.prodcastService.selectedData.name
     }
     this.webService.commonMethod('podcast/approvalstatus/admin', req, 'POST').subscribe(
       (data) => {
@@ -56,7 +56,7 @@ export class ApprovalPopupComponent implements OnInit {
         this.prodcastService.showPopUp.rejected = false;
         this.prodcastService.showPopUp.modify = false;
         if (data.Status == 'Success' && data.Response) {
-          this.toast.success('Podcast '+status+' Sucessfully');
+          this.toast.success('Podcast ' + status + ' Sucessfully');
           this.prodcastService.getDashBoardList();
         } else
           this.toast.error('Internal Server error');
@@ -71,7 +71,7 @@ export class ApprovalPopupComponent implements OnInit {
       "podcast_id": this.prodcastService.selectedData.id,
       "created_by": this.prodcastService.loginUserName,
       "broadcast_date": this.broadCastDate + ' ' + this.broadCastTime,
-      "podcast_name":this.prodcastService.selectedData.name
+      "podcast_name": this.prodcastService.selectedData.name
     }
     this.webService.commonMethod('podcast/broadcast/admin', req, 'PUT').subscribe(
       (data) => {
@@ -97,7 +97,7 @@ export class ApprovalPopupComponent implements OnInit {
       "note_description": this.notes,
       "status": this.prodcastService.selectedData.approvals,
       "created_by": "Admin",
-      "audio_path":this.prodcastService.selectedData.audiopath
+      "audio_path": this.prodcastService.selectedData.audiopath
     }
     this.webService.commonMethod('podcast/delete/admin', req, 'POST').subscribe(
       (data) => {
@@ -112,7 +112,77 @@ export class ApprovalPopupComponent implements OnInit {
       }
     )
   }
+  updateHHMM(hhmm, type) {
+    if (hhmm == 'HH') {
+      this.increaseDecreasHour(type);
+    } else {
+      if (hhmm == 'MM') {
+        this.increaseDecreaseMin(type);
+      }
+    }
+  }
+  keyUpdownHH(elem:any){
+    if(elem.keyCode == 40)
+    this.increaseDecreasHour('-');
+    if(elem.keyCode == 38)
+    this.increaseDecreasHour('+');
+  }
+  keyUpdownMM(elem:any){
+    if(elem.keyCode == 40)
+    this.increaseDecreaseMin('-');
+    if(elem.keyCode == 38)
+    this.increaseDecreaseMin('+');
+  }
+  
+    increaseDecreasHour(type){
+      if (type == "+") {
+        let num: any = Number(this.hh);
+        if (num == 23) {
+          num = "00";
+        } else {
+          num++;
+        }
+        if (num != '00' && num <= 9)
+          num = '0' + num;
+        this.hh = num;
 
+      } else {
+        let num: any = Number(this.hh);
+        if (num == 0) {
+          num = "23";
+        } else {
+          num--;
+        }
+        if (num != '23' && num <= 9)
+          num = '0' + num;
+        this.hh = num;
 
+      }
+    }
+    increaseDecreaseMin(type){
+      if (type == "+") {
+        let num: any = Number(this.mm);
+        if (num == 59) {
+          num = "00";
+        } else {
+          num++;
+        }
+        if (num != '00' && num <= 9)
+          num = '0' + num;
+        this.mm = num;
+
+      } else {
+        let num: any = Number(this.mm);
+        if (num == 0) {
+          num = "59";
+        } else {
+          num--;
+        }
+        if (num != '59' && num <= 9)
+          num = '0' + num;
+        this.mm = num;
+
+      }
+    }  
 
 }
